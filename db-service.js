a1f72ff81466fe061ca61f30aa2eb6d9a5105064
Andy Sterkowitz <andy@andysterkowitz.com>
2020-11-17 11:17:33 -0800

    Final code

--- db-service.js
+++ db-service.js
@@ -1,7 +1,7 @@
 function queryDataById(id) {
   // SELECT * FROM table WHERE id = $id;
   console.log('WARNING! YOU ARE ACCESSING A LIVE DATABASE!');
-  return { id: 1 };
+  return { id: 1, name: 'andy', email: 'andy@email.com' };
 }
 
 module.exports = {
